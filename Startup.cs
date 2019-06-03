988628792de5bbc6307667050a1fa96ffeed3e22
Shawn Lee <43838299+Skip187@users.noreply.github.com>
2019-06-02 19:14:39 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -14,58 +14,74 @@ using GroupProject3.Models;
 
 namespace GroupProject3
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
 
+			// add reference to the SQLite Database file. If the file does not exist when the migration is run, it creates the file.
+			var connectionString = "Data Source=ResidentsEntityFramework.db";
+			services.AddDbContext<ResidentsContext>(options => options.UseSqlite(connectionString));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.AddCors(options =>
+				{
+					options.AddPolicy("AllowOrigin",
+//						builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
-            // add reference to the SQLite Database file. If the file does not exist when the migration is run, it creates the file.
-            var connectionString = "Data Source=ResidentsEntityFramework.db";
-            services.AddDbContext<ResidentsContext>(options => options.UseSqlite(connectionString));
+					builder => 
+					{
+						builder.WithOrigins("http://24.164.14.197:3000","https://24.164.14.197:5001/Residents","http://localhost:3000","http://localhost:5001/Residents")
+						//	.WithHeaders("x-suiteRole-header")
+							.AllowAnyHeader()
+							.AllowAnyMethod();
+					}
+					);
+				}
+			);
 
-        }
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+				//app.UseHttpsRedirection();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+			app.UseStaticFiles();
+			app.UseCookiePolicy();
+			app.UseCors("AllowOrigin");
+//			app.UseCors();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
